e5f6529985ce152ca4de32b704ce3ad4321c0a9a
Dan Dee <monkeyjunglejuice@pm.me>
2021-12-23 13:34:50 +0800

    Add autoload

--- giorgio-theme.el
+++ giorgio-theme.el
@@ -420,6 +420,11 @@
    `(beancount-date ((t (:inherit italic :foreground nil))))
    `(beancount-account ((t (:inherit default))))))
 
+;;;###autoload
+(when load-file-name
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
+
 (provide-theme 'giorgio)
 
 ;; Local Variables:
