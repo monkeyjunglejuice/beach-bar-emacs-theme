e0a1d600fd5012809083c8dc9e2a38831e32fe47
Dan Dee <monkeyjunglejuice@pm.me>
2022-09-27 08:59:09 +0800

    Highlighting shouldn't bolden the highlighted object

--- beach-bar-theme.el
+++ beach-bar-theme.el
@@ -68,7 +68,7 @@
    `(show-paren-match ((t (:foreground ,color-fg-alt :background ,color-light :weight bold))))
    `(show-paren-match-expression ((t (:background ,color-bright-light))))
    `(show-paren-mismatch ((t (:inherit error :weight bold))))
-   `(highlight ((t (:foreground ,color-fg-alt :background ,color-light :weight bold :underline (:color ,color-fg-alt)))))
+   `(highlight ((t (:foreground ,color-fg-alt :background ,color-light :underline (:color ,color-fg-alt)))))
    `(match ((t (:inherit highlight))))
    `(hl-line ((t (:underline (:color ,color-fg-dim) :extend t))))
    `(separator-line ((t (:height 0.1 :background ,color-fg-dimmer))))
